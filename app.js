ad48756846cf5683adf15af236529391ea37781c
Tarek Gabarin <tarekgabarin1992@gmail.com>
2017-12-24 15:45:45 -0500

    fwefwefwefwefew

--- app.js
+++ app.js
@@ -64,15 +64,15 @@ db.once('open', function () {
 
 app.use(morgan('combined'));
 
-// app.use(cors());
-//
-// app.options('*', cors());
+app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.options('*', cors());
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
