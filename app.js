7837d84149fed114f18db5e97884eeffd30d8836
Tarek Gabarin <tarekgabarin1992@gmail.com>
2017-12-27 17:42:59 -0500

    Allow Methods in preflight

--- app.js
+++ app.js
@@ -71,6 +71,7 @@ app.use(morgan('combined'));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Auth, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     next();
 });
 
