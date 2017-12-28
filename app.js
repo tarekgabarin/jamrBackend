82fb87ba945691b0cc175deaaeb29460982c1715
Tarek Gabarin <tarekgabarin1992@gmail.com>
2017-12-28 15:40:29 -0500

    fergergegghgrthrth

--- app.js
+++ app.js
@@ -79,7 +79,9 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,x-auth,Accept,content-type,application/json');
+    ///res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,X-Auth,Accept,content-type,application/json');
+
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, X-Auth, X- Requested-With, Content-Type, Accept");
     next();
 });
 
