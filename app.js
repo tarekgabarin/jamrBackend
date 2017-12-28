e587b43ce4cbc6d637af04e1aec2d00234ce8c7d
Tarek Gabarin <tarekgabarin1992@gmail.com>
2017-12-28 14:22:37 -0500

    fsfwefwwefwefwefew

--- app.js
+++ app.js
@@ -74,15 +74,18 @@ app.use(morgan('combined'));
 //
 // app.options('*', cors());
 
+app.configure(function () {
 
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", '*');
+        res.header("Access-Control-Allow-Credentials", true);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,x-auth,Accept,content-type,application/json');
+        next();
+    });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,x-auth,Accept,content-type,application/json');
-    next();
 });
+
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
